ef9ef9316599ae485ffbd8592c3273ac14b9b788
EGAguirre <9eaguirre@stpatrick.org>
2016-12-12 10:54:58 -0600

    Item
    -finished item class.
    - added y own items
    -updated variables

---
+++ item.js
@@ -0,0 +1,15 @@
+class Item{
+  //Fill in the item class below this comment.
+constructor(n,p,st){
+  this.name = n;
+  this.price = p;
+  this.shippingTime = st;
+}
+
+
+}
+
+//Create your three test items below this comment.
+let mygift = new Item("Xbox",250,7);
+let yourgift = new Item("iPhone",700,5);
+let hisgift = new Item("Jacket",1000,3);
