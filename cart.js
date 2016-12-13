72011e418a7c8c3288eaca94d281ca3d43656f39
EGAguirre <9eaguirre@stpatrick.org>
2016-12-13 10:50:11 -0600

    added cart
    - didn't finish
    -add the functions

---
+++ cart.js
@@ -0,0 +1,21 @@
+class Cart{
+  //What is the first part of every class? Type it below.
+constructor(iq,il){
+  this.itemQuantity = iq;
+  this.itemList = il;
+}
+
+
+  //Type the instance functions below this comment.
+addItem(i,q){
+
+}
+totalCart(){
+  var total = this.itemList + this.price;
+  return total;
+}
+subCart(days){
+
+}
+
+}
