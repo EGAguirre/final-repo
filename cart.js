a11cdffd354655cc1cf8782913f5ad81e5816986
EGAguirre <9eaguirre@stpatrick.org>
2016-12-14 11:26:22 -0600

    cart
    -finished cart
    -remodel a bit
    -recheck other functions

--- cart.js
+++ cart.js
@@ -1,21 +1,30 @@
-class Cart{
-  //What is the first part of every class? Type it below.
-constructor(iq,il){
-  this.itemQuantity = iq;
-  this.itemList = il;
-}
+  class Cart{
+    //What is the first part of every class? Type it below.
+  constructor(il,iq){}
+   this.itemList = il;
+   this.quantity = iq;
+  }
 
 
-  //Type the instance functions below this comment.
-addItem(i,q){
+    //Type the instance functions below this comment.
+    addItem(i,q){
+      this.itemList.push(i);
+      this.itemQuantity.push(q);
+    }
 
-}
-totalCart(){
-  var total = this.itemList + this.price;
-  return total;
-}
-subCart(days){
+    totalCart(){
+    let total = 0;
+    for(var i=0;i < quantity.length;i++){
+      total += this.itemList[i] x this.itemQuantity[i]
+    }
+    return total;
+  }
 
-}
 
-}
+   subCart(days){
+    let subArray =[]
+    if (Item.shippingTime == day){
+      subArray.push(Item.name);
+    }
+    return subArray;
+  }
