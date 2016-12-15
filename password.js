6232fe8a0e9e385783093cfcc66687a30a4f20ab
EGAguirre <9eaguirre@stpatrick.org>
2016-12-15 10:52:46 -0600

    need to finish static
    -finished everything else

--- password.js
+++ password.js
@@ -1,11 +1,43 @@
 class Password{
   //The first part of the class.
-
+constructor(pb,pv){
+this.publicKey=pb;
+this.privateKey=pv;
+}
 
 
   //Instance functions below this comment.
+validPublicKey(){
+  if(this.publicKey.length>=8 && this.publicKey.length<=25){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+validPrivateKey(){
+    if(this.privateKey[4]="-" && this.privateKey[9]="-"){
+      return true;
+    }
+    else{
+      return false;
+    }
+
+    static makePrivateKey(){
+      let key = [];
+      let limit = 14;
+
+      for(var i=0, i<14, i++){
+        if (!i==4 && !==9){
+          password.push(Math.floor(Math.Random()*10));
+      }
+      else{
+        password.push("-")
+      }
+  }
+  return passowrd
+}
 
 
-
-  //Static function below this comment.
 }
